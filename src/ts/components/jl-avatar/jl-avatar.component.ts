aaee3ce8023d68bf3608f329a456c2492d9f8801
Pascal Garber <pascal@artandcode.studio>
2024-02-08 15:13:26 +0100

    Show random avatar and toggle on click

--- src/ts/components/jl-avatar/jl-avatar.component.ts
+++ src/ts/components/jl-avatar/jl-avatar.component.ts
@@ -1,7 +1,9 @@
 import { Component } from "@ribajs/core";
 import { hasChildNodesTrim } from "@ribajs/utils/src/dom.js";
 
-import imageSrc from "../../../assets/avatar_03.jpg?url";
+import avatar01 from "../../../assets/avatar_01.jpg?url";
+import avatar02 from "../../../assets/avatar_02.jpg?url";
+import avatar03 from "../../../assets/avatar_03.jpg?url";
 
 export class JLAvatarComponent extends Component {
   public static tagName = "jl-avatar";
@@ -11,12 +13,32 @@ export class JLAvatarComponent extends Component {
   }
 
   public scope = {
-    imageSrc,
+    images: [avatar01, avatar02, avatar03],
+    activeIndex: 0,
+    toggleIndex: this.toggleIndex,
   };
+
+  constructor() {
+    super();
+    this.scope.activeIndex = this.getRandomNumberInRange(0, this.scope.images.length - 1);
+  }
+
+  public toggleIndex() {
+    this.scope.activeIndex++;
+    if(this.scope.activeIndex > this.scope.images.length - 1) {
+      this.scope.activeIndex = 0;
+    }
+  }
 
   protected connectedCallback() {
     super.connectedCallback();
     this.init(JLAvatarComponent.observedAttributes);
+  }
+
+  protected getRandomNumberInRange(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   protected requiredAttributes(): string[] {
